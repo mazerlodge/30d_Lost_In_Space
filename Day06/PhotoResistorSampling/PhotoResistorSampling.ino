7707e3c054220195dc372d7d1d71fe2a3a42176c
mazerlodge <mazerlodge@hotmail.com>
2023-04-22 18:46:11 -0500

    Comments added, only output to serial on val change +/- 2.

--- Day06/PhotoResistorSampling/PhotoResistorSampling.ino
+++ Day06/PhotoResistorSampling/PhotoResistorSampling.ino
@@ -1,27 +1,45 @@
+/*
+30d Lost in Space Day 6
 
+	Read a photo resistor.
+	Blink the onboard LED for a period equal to the sensor value.
+	More light it slower blink (longer on and off interval).
+	
+Revision History:
+	20230122 - Initial version
+	20230422 - Mod to only output to the serial port if the sensor value changed by +/-2.
+			 - Comments added.
+
+*/
 int photoSensePin = A0;
 int onboardLED = 13;
 int photoSenseValue = 0;
-
+int prevSenseValue = -1;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(onboardLED, OUTPUT); 
-  
+	// put your setup code here, to run once:
+	Serial.begin(9600);
+	pinMode(onboardLED, OUTPUT); 
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  photoSenseValue = analogRead(photoSensePin); 
+	// Get the sensor value, more light = higher number.
+	photoSenseValue = analogRead(photoSensePin); 
 
-  digitalWrite(onboardLED, HIGH);
-  delay(photoSenseValue);
-  digitalWrite(onboardLED, LOW);
-  delay(photoSenseValue);
+	// light the onboard LED for time relative to sensor value.
+	digitalWrite(onboardLED, HIGH);
+	delay(photoSenseValue);
+	digitalWrite(onboardLED, LOW);
+	delay(photoSenseValue);
 
-  Serial.println(photoSenseValue);
-  delay(photoSenseValue);
- 
+	// Only output to the serial console if the sensor value changed.
+	if ((photoSenseValue < prevSenseValue - 2)
+		|| (photoSenseValue > prevSenseValue + 2)) { 
+		prevSenseValue = photoSenseValue;
+		Serial.println(photoSenseValue);
+		delay(photoSenseValue);
+
+	}
 
 }
